18790519a999c9d832903ef5dd82b4d5e5302de2
MarianF91 <marian_f91@yahoo.com>
2024-12-06 11:20:31 +0200

    Reorganized files and added "Ana vrea mere." & "Ana nu mai vrea mere."

---
+++ src/course4/lesson/Main.java
@@ -0,0 +1,11 @@
+package course4.lesson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ana vrea mere.");
+        System.out.println("Ana nu mai vrea mere");
+
+    }
+}
