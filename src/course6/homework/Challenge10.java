c01df6d29415d7dfc8c68af6db0595c604d0a372
MarianF91 <marian_f91@yahoo.com>
2024-12-16 10:01:50 +0200

    Course 6 Challenge 10;

---
+++ src/course6/homework/Challenge10.java
@@ -0,0 +1,17 @@
+package course6.homework;
+
+public class Challenge10 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int additionResult = a+b;
+        int multiplicationResult = c*d;
+
+        System.out.println("Addition result: " + additionResult);
+        System.out.println("Multiplication result: " + multiplicationResult);
+    }
+}
