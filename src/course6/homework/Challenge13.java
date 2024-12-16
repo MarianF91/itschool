2e28248c0302c2e58d7dacd6bf7ae795bda932ff
MarianF91 <marian_f91@yahoo.com>
2024-12-16 11:10:33 +0200

    Course 6 Challenge 13

---
+++ src/course6/homework/Challenge13.java
@@ -0,0 +1,15 @@
+package course6.homework;
+
+public class Challenge13 {
+
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        System.out.println("The original number is: " + number);
+        number++;
+        System.out.println("After using ++ it becomes: " + number);
+        number--;
+        System.out.println("After using -- on the new value, it becomes: " + number);
+    }
+}
