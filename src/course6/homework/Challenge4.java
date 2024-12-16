c275eb8d7d6aa3a490cd992deb9de4f80480bfa0
MarianF91 <marian_f91@yahoo.com>
2024-12-16 08:30:21 +0200

    Course 6 Challenge 4 rework

--- src/course6/homework/Challenge4.java
+++ src/course6/homework/Challenge4.java
@@ -1,14 +1,27 @@
 package course6.homework;
 
+import java.util.Scanner;
+
 public class Challenge4 {
 
     public static void main(String[] args) {
 
-        int firstHeight = 10;
-        int secondHeight = 12;
+        Scanner scan = new Scanner(System.in);
 
-        String maximumHeight = (firstHeight > secondHeight) ? ("firstHeight has the higher value, because " + (firstHeight + " > " + secondHeight))
-        : ("secondHeight has the higher value, because " + (secondHeight + " > " + firstHeight));
+        System.out.println("Enter the first height (without using any commas):");
+        int firstHeight = scan.nextInt();
+
+        System.out.println("Enter the second height (without using any commas):");
+        int secondHeight = scan.nextInt();
+
+        if (firstHeight == secondHeight) {
+            System.out.println("The values must be different.");
+            
+            System.exit(1);
+        }
+        
+        String maximumHeight = (firstHeight > secondHeight) ? ((firstHeight + "is the higher value, because ") + (firstHeight + " > " + secondHeight))
+        : ((secondHeight + " is the higher value, because ") + (secondHeight + " > " + firstHeight));
 
         System.out.println(maximumHeight);
     }
