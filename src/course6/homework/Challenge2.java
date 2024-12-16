3efb2584f1578c2580037aa64314e725a720e6bd
MarianF91 <marian_f91@yahoo.com>
2024-12-16 07:56:18 +0200

    Course 6 Challenge2 and Challenge 3

---
+++ src/course6/homework/Challenge2.java
@@ -0,0 +1,16 @@
+package course6.homework;
+
+public class Challenge2 {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 12;
+        int secondNumber = 12;
+
+        String firstString = "12";
+        String secondString = "12";
+
+        System.out.println("Are numbers equal? " + (firstNumber == secondNumber));
+        System.out.println("Are strings equal? "+ (firstString.equals(secondString)));
+    }
+}
