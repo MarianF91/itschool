9cf9c596c4e25aea2f3f57f2e9d908f088100bbc
MarianF91 <marian_f91@yahoo.com>
2025-01-20 10:24:35 +0200

    Course 11, Recap Loops and Flow Control, recaps 1, 2, 3, 8, 10;

---
+++ src/recap/loops/and/flow/control/Recap3.java
@@ -0,0 +1,26 @@
+package recap.loops.and.flow.control;
+import java.util.Scanner;
+
+public class Recap3 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("The factorial of a number");
+        System.out.println("Enter a positive number:");
+        int number = scan.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Invalid input.");
+            System.exit(1);
+        }
+
+        int factorial = number;
+
+        for (int i = 2; i <= number; i++) {
+            factorial *= i - 1;
+        }
+        System.out.println("The factorial of the number " + number + " is " + factorial + ".");
+    }
+}
---
+++ src/recap/loops/and/flow/control/Recap8.java
@@ -0,0 +1,30 @@
+package recap.loops.and.flow.control;
+
+import java.util.Scanner;
+
+public class Recap8 {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("This simulates a Fibonacci series. Choose the value for n.");
+
+        int n = scanner.nextInt();
+
+        if (n < 1 ){
+            System.out.print("Number too low.");
+            System.exit(1);
+        }
+
+        System.out.print("Your Fibonacci series is: " + firstNumber + " ," + secondNumber + ", ");
+        for (int i = 0; i < n; i++){
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber + secondNumber;
+            System.out.print(firstNumber + " ," + secondNumber + ", ");
+        }
+    }
+}
