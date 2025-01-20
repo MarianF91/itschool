f5ad23d48802c23f5f779079ae313b86cd21b48b
MarianF91 <marian_f91@yahoo.com>
2025-01-20 08:32:22 +0200

    Course 11, Recap Java Basics, recaps 3, 4, 6, 7, 8

---
+++ src/recap/java/basics/Recap6.java
@@ -0,0 +1,30 @@
+package recap.java.basics;
+
+import java.util.Scanner;
+
+public class Recap6 {
+
+    public static void main(String[] args) {
+
+        String string;
+        int fullNumber;
+        double digitNumber;
+        boolean isTrueOrFalse;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a string.");
+        string = scan.nextLine();
+
+        System.out.println("Enter a number without digits.");
+        fullNumber = scan.nextInt();
+
+        System.out.println("Enter a number with digits.");
+        digitNumber = scan.nextDouble();
+
+        System.out.println("Simply enter <<true>> or <<false>>.");
+        isTrueOrFalse = scan.nextBoolean();
+
+        System.out.println("These variables are:\n- a string: " + string + "\n- an int: " + fullNumber + "\n- a double: "
+                + digitNumber + "\n- a boolean: " + isTrueOrFalse);
+    }
+}
