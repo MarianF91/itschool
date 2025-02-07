1fd572de35c2c81754d0da4bb2aba9a70d3b47ed
MarianF91 <marian_f91@yahoo.com>
2025-02-07 05:11:15 +0200

    refactoring packs

---
+++ src/course11recap/loopsandflowcontrol/Recap7.java
@@ -0,0 +1,38 @@
+package course11recap.loopsandflowcontrol;
+
+import java.util.Scanner;
+
+public class Recap7 {
+
+    public static void main(String[] args) {
+
+        System.out.println("This code returns the sum of even numbers (except 0).");
+        System.out.println("The sum of all the even numbers is: " + evenNumbersSum());
+
+    }
+
+    public static double evenNumbersSum() {
+
+        Scanner scan = new Scanner(System.in);
+        double number;
+        double sum = 0;
+
+        while (true) {
+
+            System.out.println("Enter a new number.");
+            number = scan.nextDouble();
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number % 2 != 0) {
+                continue;
+            }
+
+            sum += number;
+
+        }
+        return sum;
+    }
+}
---
+++ src/course11recap/stringmanipulation/Recap8.java
@@ -0,0 +1,45 @@
+package course11recap.stringmanipulation;
+
+import java.util.Scanner;
+
+public class Recap8 {
+
+    public static void main(String[] args) {
+
+        String compressedString = stringCompression();
+        System.out.println("The compressed string is: " + compressedString);
+
+    }
+
+    public static String stringCompression() {
+
+        String originalString;
+        Scanner scan = new Scanner(System.in);
+        StringBuilder compressionString = new StringBuilder();
+        int charCounter = 1; //variable used to count the number of times a char was printed;
+
+        System.out.println("Enter a string (ex:aaabbbccc).");
+        originalString = scan.nextLine();
+
+        for (int i = 0; i < originalString.length(); i++) {
+
+            char currentChar = originalString.charAt(i);
+
+            if (i > 0 && currentChar == originalString.charAt(i - 1)) {
+                //checks if the current char and previous char are equal and starts counting;
+                charCounter++;
+            } else {
+                if (charCounter > 1) {
+                    compressionString.append(charCounter);
+                }
+                compressionString.append(currentChar);
+                charCounter = 1;
+            }
+        }
+        // Handle the last character count
+        if (charCounter > 1) {
+            compressionString.append(charCounter);
+        }
+        return compressionString.toString();
+    }
+}
