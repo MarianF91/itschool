558fb35b3eae620c86fe1d14e571bddce1005c6d
MarianF91 <marian_f91@yahoo.com>
2025-03-06 14:14:50 +0200

    Course 19 Frequent Items in a Shopping Cart challenge.

---
+++ src/course19/homework/frequentitemsinashoppingcart/Product.java
@@ -0,0 +1,13 @@
+package course19.homework.frequentitemsinashoppingcart;
+
+public enum Product {
+
+    MILK,
+    LOW_FAT_MILK,
+    NONFAT_MILK,
+    GOAT_CHEESE,
+    COW_CHEESE,
+    SHEEP_CHEESE,
+    CHEDDAR,
+    SMOKED_CHEESE,
+}
