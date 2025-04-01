d2f81cd85c9ff03fea5ade713e9fb8f9caaa3ebc
MarianF91 <marian_f91@yahoo.com>
2025-04-01 19:34:06 +0300

    Course 22 - changes

--- src/course20and21/Challenge10.java
+++ src/course20and21/Challenge10.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class Challenge10 {
     public static void main(String[] args) {
-    Integer[] numbers = {1, 2, 3};
-    Character[] letters = {'a', 'b', 'c'};
+        Integer[] numbers = {1, 2, 3};
+        Character[] letters = {'a', 'b', 'c'};
         System.out.println("Numbers array: " + Arrays.toString(numbers));
         System.out.println("Char array: " + Arrays.toString(letters));
 
-        elementSwap(numbers, 0,2);
-        elementSwap(letters, 0,1);
+        elementSwap(numbers, 0, 2);
+        elementSwap(letters, 0, 1);
 
         System.out.println("Swapped arrays: " + "\n- numbers: " + Arrays.toString(numbers)
-                +"\n- letters: " + Arrays.toString(letters));
+                + "\n- letters: " + Arrays.toString(letters));
     }
 
     public static <T> void elementSwap(T[] array, int index1, int index2) {
